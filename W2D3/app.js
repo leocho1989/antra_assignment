a6f9cfd17d070cfbf2dfecff5d8d2fdbf1db2cf9
Leo <leo.cho1989@gmail.com>
2022-05-25 20:33:34 -0700

    second commit10

---
+++ W2D3/app.js
@@ -0,0 +1,20 @@
+
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((todos) => {
+
+      console.log(todos);
+    const todoList = document.getElementById("todo-list");
+        todoList.innerHTML = "";
+    // console.log(todoList);
+        todos.map((todo) => todo.value = todoList.innerHTML += 
+        `<div class="todo_box">
+        <li>  UserId: ${todo.userId} </li>
+        <li> Todo Id: ${todo.id} </li>
+        <li> Todo Title: ${todo.title} </li>
+        <li> Completed: ${todo.completed} </li>
+        </div>
+        ` );
+
+    });
